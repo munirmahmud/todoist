933f9a0c3faa2bb67edeba362743d1e363a98972
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-31 10:22:18 +0600

    Built the checkbox component & showed the demo task

--- src/components/layouts/Content.js
+++ src/components/layouts/Content.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Tasks } from '../Tasks';
 import { Sidebar } from './Sidebar';
 
 const Content = () => {
     return (
         <section>
             <Sidebar />
+            <Tasks />
         </section>
     )
 }
