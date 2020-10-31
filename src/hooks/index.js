bb283ebba45d8e487869936ac745294530cdb567
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-31 10:18:52 +0600

    Custom hook for projects to get all the projects added

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -39,4 +39,27 @@ export const useTasks = selectedProject => {
 };
 
 
+export const useProjects = () => {
+    const [projects, setProjects] = useState([]);
 
+    useEffect(() => {
+        firebase.firestore()
+            .collection('projects')
+            .where('userId', '==', 'w0qnOplDcbUDFCMCvVCQ')
+            .orderBy('projectId')
+            .get()
+            .then(snapshot => {
+                const allProjects = snapshot.docs.map(project => ({
+                    ...project.data(),
+                    docId: project.id,
+                }));
+
+                if(JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+                    setProjects(allProjects);
+                }
+            });
+
+    }, [projects]);
+
+    return { projects, setProjects };
+};
